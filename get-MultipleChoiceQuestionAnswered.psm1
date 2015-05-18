5f54ccf19edaa8f667d0e30a7090a04bb949e1a9
ChrisMagnuson <hourglasssand@gmail.com>
2015-05-18 09:41:39 -0400

    Initial commit

---
+++ get-MultipleChoiceQuestionAnswered.psm1
@@ -0,0 +1,19 @@
+﻿function get-MultipleChoiceQuestionAnswered {
+    [CmdletBinding()]
+    param(
+        $Question,
+        $Choices
+    )
+    
+    $ChoiceDescriptions = @()
+    foreach ($Choice in $Choices) {
+        $ChoiceDescriptions += New-Object System.Management.Automation.Host.ChoiceDescription "&$Choice"
+    }
+
+    $options = [System.Management.Automation.Host.ChoiceDescription[]]$ChoiceDescriptions
+
+    $result = $host.ui.PromptForChoice("", $Question, $options, 0)
+    Write-Verbose "Index of result chosen: $result"
+    Write-Verbose "Choice selected: $($Choices[$result])"
+    $Choices[$result]
+}
