a08a0664812aeb77e9798eca0e912dd491e07f7e
ChrisMagnuson <hourglasssand@gmail.com>
2015-06-05 16:10:09 -0400

    Updated function to support setting a default choice

--- get-MultipleChoiceQuestionAnswered.psm1
+++ get-MultipleChoiceQuestionAnswered.psm1
@@ -2,7 +2,8 @@
     [CmdletBinding()]
     param(
         $Question,
-        $Choices
+        [array]$Choices,
+        $DefaultChoice
     )
     
     $ChoiceDescriptions = @()
@@ -12,7 +13,7 @@
 
     $options = [System.Management.Automation.Host.ChoiceDescription[]]$ChoiceDescriptions
 
-    $result = $host.ui.PromptForChoice("", $Question, $options, 0)
+    $result = $host.ui.PromptForChoice("", $Question, $options, $Choices.IndexOf($DefaultChoice))
     Write-Verbose "Index of result chosen: $result"
     Write-Verbose "Choice selected: $($Choices[$result])"
     $Choices[$result]
